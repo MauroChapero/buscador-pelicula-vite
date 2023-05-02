3b1bb07b4cf6c7f37068ffd9fac84e0b4895b917
MauroChapero <chaperomauro1803@gmail.com>
2023-05-02 20:05:19 -0300

    Project Init with Eslint config

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1>Cinefilo</h1>
+        <h2>Buscador de Peliculas</h2>
+        <div>
+          <form>
+            <input type="text" placeholder="Buscar pelicula"/>
+            <button type="submit">Buscar</button>
+          </form>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
